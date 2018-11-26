079a641040dc4e0d3bf4863d79844c35046e9ffe
Dariell Vasquez <dariellv@yahoo.com>
2018-11-26 14:02:08 -0500

    postReducer completed, list of posts rendering

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,13 +4,34 @@ import {fetchPosts} from '../actions';
 
 class PostList extends Component{
     componentDidMount(){
-        console.log(this.props.fetchPosts());
+        this.props.fetchPosts();   
+    }
+
+    renderPosts = () => {
+        return this.props.posts.map(post=>{
+                return (
+                    <div className="item" key={post.id}>
+                        <i className="large middle aligned icon user" />
+                        <div className="content">
+                            <div className="description">
+                                <h2>{post.title}</h2>
+                                <p>{post.body}</p>
+                            </div>
+                        </div>   
+                    </div>
+                )
+            })
     }
     render(){
         return(
-            <div>Post list</div>
+            <div className="ui relaxed divided list">
+                    {this.renderPosts()}
+            </div>
         )
     }
 }
 
-export default connect(null, {fetchPosts})(PostList);
+const mapStateToProps = (state) =>{
+    return {posts: state.posts}
+}
+export default connect(mapStateToProps, {fetchPosts})(PostList);
